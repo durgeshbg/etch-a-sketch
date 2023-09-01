c25908d5f3acfd948e85b7fd833ac77f4d640566
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-09-02 00:03:04 +0530

    create 16x16 board divs with html classes

---
+++ script.js
@@ -0,0 +1,15 @@
+const board = document.querySelector('.board');
+const divs = Array.from(new Array(16), () => new Array(16));
+
+for (let i = 0; i < 16; i++)
+    for (let j = 0; j < 16; j++) divs[i][j] = document.createElement('div');
+
+divs.forEach((divRow) => {
+    const boardRow = document.createElement('div');
+    boardRow.classList.add('board-row');
+    divRow.forEach((divItem) => {
+        divItem.classList.add('board-item');
+        boardRow.appendChild(divItem);
+    });
+    board.appendChild(boardRow);
+});
