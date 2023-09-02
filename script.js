be96694202126d0dd7ed40e330b536376ccaec78
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-09-02 19:35:30 +0530

    add resize board feature

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let color = 'black';
-// let size = prompt("Enter grid size: ");
 let size = 16;
 let board = document.querySelector('.board');
 
@@ -24,13 +23,24 @@ function clearBoard() {
     );
 }
 
+function resizeBoard() {
+    do {
+        size = prompt("Enter size: ");
+        if (size > 64) alert("Size too big: Optimal Range 10-64");
+        else if(size < 10) alert("Size too small: Optimal Range 10-64")
+    } while (size < 10 || size > 64)
+    clearBoard();
+    createBoard(size);
+}
+
+
+createBoard(size);
+
 document.querySelectorAll('.pallete-buttons').forEach((button) => {
     button.addEventListener('click', (e) => {
         color = e.target.dataset.color;
     });
 });
-
-createBoard(size);
 
 window.addEventListener('keydown', (e) => {
     if (e.code == 'Space') clearBoard();
