e9a44649ad81f78abac4d71bd030ef90db2fce1d
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-09-02 13:11:04 +0530

    refactor code

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 const board = document.querySelector('.board');
-const divs = Array.from(new Array(16), () => new Array(16));
+let color = 'black';
+let size = 16;
+let itemWidth = `${800 / size}px`;
+let itemHeigth = `${800 / size}px`;
 
-for (let i = 0; i < 16; i++)
-    for (let j = 0; j < 16; j++) divs[i][j] = document.createElement('div');
-
-divs.forEach((divRow) => {
-    const boardRow = document.createElement('div');
-    boardRow.classList.add('board-row');
-    divRow.forEach((divItem) => {
-        divItem.classList.add('board-item');
-        boardRow.appendChild(divItem);
+for (let i = 0; i < size * 16; i++) {
+    const divItem = document.createElement('div');
+    divItem.style.width = itemWidth;
+    divItem.style.height = itemHeigth;
+    divItem.addEventListener('mouseover', () => {
+        divItem.style.background = color;
     });
-    board.appendChild(boardRow);
-});
-
-board.addEventListener('mouseover', (e) => {
-    if (e.target.classList.value === 'board-item') {
-        e.target.classList.add('changed');
-    }
-});
+    board.appendChild(divItem);
+}
