7acf333bfaf4a80d2a1d72d812a10f749522210b
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-09-02 16:28:56 +0530

    add feature to change color

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
-const board = document.querySelector('.board');
 let color = 'black';
 let size = 16;
-let itemWidth = `${800 / size}px`;
-let itemHeigth = `${800 / size}px`;
+const board = document.querySelector('.board');
 
-for (let i = 0; i < size * 16; i++) {
-    const divItem = document.createElement('div');
-    divItem.style.width = itemWidth;
-    divItem.style.height = itemHeigth;
-    divItem.addEventListener('mouseover', () => {
-        divItem.style.background = color;
-    });
-    board.appendChild(divItem);
+function createBoard(size) {
+    let itemWidth = `${800 / size}px`;
+    let itemHeigth = `${800 / size}px`;
+
+    for (let i = 0; i < size * size; i++) {
+        const divItem = document.createElement('div');
+        divItem.style.width = itemWidth;
+        divItem.style.height = itemHeigth;
+        divItem.addEventListener('mouseover', () => {
+            divItem.style.background = color;
+        });
+        board.appendChild(divItem);
+    }
+    return board;
 }
+
+// function resetBoard(board) {
+//     board.forEach(boardItem => boardItem.style.background = 'white');
+// }
+
+document.querySelectorAll('.pallete-buttons').forEach(button => {
+    button.addEventListener('click', (e) => {
+        color = e.target.dataset.color;
+    })
+})
+
+createBoard(size);
+
+window.addEventListener('keydown', (e) => {
+    if (e.code == 'Space') {
+        console.log('Hello');
+    }
+});
