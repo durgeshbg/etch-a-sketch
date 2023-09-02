76174b92a17a1cb2826f6cffd777e509c53bc47b
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-09-02 19:19:25 +0530

    add board reset feature

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let color = 'black';
+// let size = prompt("Enter grid size: ");
 let size = 16;
-const board = document.querySelector('.board');
+let board = document.querySelector('.board');
 
 function createBoard(size) {
     let itemWidth = `${800 / size}px`;
@@ -15,23 +16,22 @@ function createBoard(size) {
         });
         board.appendChild(divItem);
     }
-    return board;
 }
 
-// function resetBoard(board) {
-//     board.forEach(boardItem => boardItem.style.background = 'white');
-// }
+function clearBoard() {
+    board.childNodes.forEach(
+        (boardItem) => (boardItem.style.background = 'white')
+    );
+}
 
-document.querySelectorAll('.pallete-buttons').forEach(button => {
+document.querySelectorAll('.pallete-buttons').forEach((button) => {
     button.addEventListener('click', (e) => {
         color = e.target.dataset.color;
-    })
-})
+    });
+});
 
 createBoard(size);
 
 window.addEventListener('keydown', (e) => {
-    if (e.code == 'Space') {
-        console.log('Hello');
-    }
+    if (e.code == 'Space') clearBoard();
 });
