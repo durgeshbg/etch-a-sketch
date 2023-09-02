0eefc7068c3e2790e3ab2229829bba2e145b515f
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-09-02 21:32:49 +0530

    update input type
    Have to click and hold the mouse to update the grid

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let color = 'black';
 let size = 16;
-let board = document.querySelector('.board');
+let isClicked = false;
+const board = document.querySelector('.board');
 
 function createBoard(size) {
     let itemWidth = `${800 / size}px`;
@@ -11,7 +12,13 @@ function createBoard(size) {
         divItem.style.width = itemWidth;
         divItem.style.height = itemHeigth;
         divItem.addEventListener('mouseover', () => {
-            divItem.style.background = color;
+            if (isClicked) divItem.style.background = color;
+        });
+        divItem.addEventListener('mousedown', () => {
+            isClicked = true;
+        });
+        divItem.addEventListener('mouseup', () => {
+            isClicked = false;
         });
         board.appendChild(divItem);
     }
@@ -25,16 +32,13 @@ function clearBoard() {
 
 function resizeBoard() {
     do {
-        size = prompt("Enter size: ");
-        if (size > 64) alert("Size too big: Optimal Range 10-64");
-        else if(size < 10) alert("Size too small: Optimal Range 10-64")
-    } while (size < 10 || size > 64)
-    clearBoard();
+        size = prompt('Enter size: ');
+        if (size > 64) alert('Size too big: Optimal Range 10-64');
+        else if (size < 10) alert('Size too small: Optimal Range 10-64');
+    } while (size < 10 || size > 64);
+    while (board.hasChildNodes()) board.removeChild(board.firstChild);
     createBoard(size);
 }
-
-
-createBoard(size);
 
 document.querySelectorAll('.pallete-buttons').forEach((button) => {
     button.addEventListener('click', (e) => {
@@ -45,3 +49,5 @@ document.querySelectorAll('.pallete-buttons').forEach((button) => {
 window.addEventListener('keydown', (e) => {
     if (e.code == 'Space') clearBoard();
 });
+
+createBoard(size);
